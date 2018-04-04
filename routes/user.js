ecc53b666e490ab47e28f68cc9cca5e44edb8c1a
sorita114 <sorita114@gmail.com>
2018-04-04 09:11:48 +0900

    user api key edit

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ var CONTACTS_COLLECTION = 'user';
 
 var db;
 
-mongodb.MongoClient.connect('mongodb://heroku_cjng4mtc:10426sori@ds231719.mlab.com:31719/heroku_cjng4mtc', function (err, client) {
+mongodb.MongoClient.connect('mongodb://heroku_cjng4mtc:o5g6v2q3v4ebeu6s8pob01vjf6@ds231719.mlab.com:31719/heroku_cjng4mtc', function (err, client) {
 	if(err) {
 		console.log(err);
 		process.exit(1);
